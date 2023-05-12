18f88670f55ae6770c335d420e7d897b8c9c2932
vinay <vinay@youngman.co.in>
2023-05-12 04:40:58 +0000

    fixed endPoint

--- models/res_config_settings.py
+++ models/res_config_settings.py
@@ -6,6 +6,7 @@ from odoo import models, fields, api
 class ResConfigSettings(models.TransientModel):
     _inherit = 'res.config.settings'
     beta_db_url = fields.Char(string='Beta DB Url', config_parameter='ym_beta_updates.beta_db_url')
+    endpoint = fields.Char(string='Beta DB Endpoint', config_parameter='ym_beta_updates.endpoint')
     beta_db_port = fields.Char(string='Beta DB Port', config_parameter='ym_beta_updates.beta_db_port')
     beta_db = fields.Char(string='Beta DB Name', config_parameter='ym_beta_updates.beta_db')
     beta_db_username = fields.Char(string='Beta DB Username', config_parameter='ym_beta_updates.beta_db_username')
