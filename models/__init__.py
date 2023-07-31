3ee80fd439cc1859eca7b081139215529253794b
satewalatul <atul.satewal@thesafetyfirst.in>
2023-07-31 17:55:44 +0530

    fixed somethings

--- models/__init__.py
+++ models/__init__.py
@@ -1,1 +1,1 @@
-from . import res_config_settings, partner_inherit, sale_order_inherit, customer_to_beta,jobsite_details
+from . import res_config_settings, partner_inherit, sale_order_inherit, customer_to_beta
