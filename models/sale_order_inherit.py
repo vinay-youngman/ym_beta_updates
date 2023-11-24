d09c9ca19dedf47163c729acfc7c445b90d63f85
Ashish Kumar Chaurasiya <102511076+achaurasiyaa@users.noreply.github.com>
2023-11-24 16:20:28 +0530

    emerging (#24)
    * approval_work_ymbeta
    
    * emerging
    
    ---------
    
    Co-authored-by: vinay <vinay@youngman.co.in>

--- models/sale_order_inherit.py
+++ models/sale_order_inherit.py
@@ -308,6 +308,9 @@ class SaleOrderInherit(models.Model):
             raise UserError(_(e))
 
     def action_confirm(self):
+        if self.is_sale_order_approval_required:
+            super(SaleOrderInherit, self).action_confirm()
+            return
         self._validate_order_before_confirming()
         self.env['customer.to.beta']._create_customer_in_beta_if_not_exists(self.partner_id)
         self._create_branch_in_beta_if_not_exists() #For branches that were added post initial customer creation
